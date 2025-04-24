c09981f7dd739e9ff08a03c90d9d77efbeb53f55
HolliverCosta <holliver.costa@ccc.ufcg.edu.br>
2025-04-24 15:40:04 -0300

    feat: refactor updateProduct function to use APIGatewayProxyEventV2 and improve response handling

--- src/functions/updateProduct.ts
+++ src/functions/updateProduct.ts
@@ -1,12 +1,34 @@
-import type { APIGatewayProxyResult } from 'aws-lambda';
+import { UpdateCommand } from '@aws-sdk/lib-dynamodb';
+import type { APIGatewayProxyEventV2 } from 'aws-lambda';
+import { dynamoClient } from 'src/lib/dynamoClient';
 
-export async function handler(event: APIGatewayProxyResult) {
-  console.log(event)
+export async function handler(event: APIGatewayProxyEventV2) {
+  const { productId } = event.pathParameters;
+  const body = JSON.parse(event.body);
+
+  const command = new UpdateCommand({
+      TableName: 'ProductsTable',
+      Key:{
+        id: productId
+      },
+
+      UpdateExpression: 'set #name = :name, #price = :price, #tags = :tags',
+      ExpressionAttributeNames: {
+        '#name': 'name',
+        '#price': 'price',
+        '#tags': 'tags'
+      },
+      ExpressionAttributeValues: {
+        ':name': body.name,
+        ':price': body.price,
+        ':tags': body.tags
+      },
+    })
+
+  const response = await dynamoClient.send(command);
 
   return {
     statusCode: 200,
-    body: JSON.stringify({
-      message: 'updateProduct',
-  })
+    body: JSON.stringify(response)
   }
 }
