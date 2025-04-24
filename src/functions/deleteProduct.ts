0aaf08e3136e139ca65fd0bd8fc16a4922720b53
HolliverCosta <holliver.costa@ccc.ufcg.edu.br>
2025-04-24 15:31:08 -0300

    feat: implement deleteProduct function with DynamoDB integration

--- src/functions/deleteProduct.ts
+++ src/functions/deleteProduct.ts
@@ -1,12 +1,20 @@
-import type { APIGatewayProxyResult } from 'aws-lambda';
+import { DeleteCommand } from '@aws-sdk/lib-dynamodb';
+import type { APIGatewayProxyEventV2 } from 'aws-lambda';
+import { dynamoClient } from 'src/lib/dynamoClient';
 
-export async function handler(event: APIGatewayProxyResult) {
-  console.log(event)
+export async function handler(event: APIGatewayProxyEventV2) {
+  const { productId } = event.pathParameters;
+
+  const command = new DeleteCommand({
+    TableName: 'ProductsTable',
+    Key: {
+     id: productId
+    }
+  })
+
+  await dynamoClient.send(command);
 
   return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'deleteProduct',
-  })
+    statusCode: 204,
   }
 }
